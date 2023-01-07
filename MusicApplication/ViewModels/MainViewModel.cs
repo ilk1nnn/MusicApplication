bbd46cd65c2eb16c61a51e9ff807881afbfdfe1e
Sule_jr92 <Sule_jr92@STHQ012B-07>
2023-01-07 17:32:52 +0400

    Add project files.

---
+++ MusicApplication/ViewModels/MainViewModel.cs
@@ -0,0 +1,39 @@
+﻿using MusicApplication.Commands;
+using MusicApplication.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MusicApplication.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public RelayCommand PlusButtonCommand { get; set; }
+
+        private WrapPanel myWrapPanel;
+
+        public WrapPanel MyWrapPanel
+        {
+            get { return myWrapPanel; }
+            set { myWrapPanel = value; OnPropertyChanged(); }
+        }
+
+
+
+
+        public MainViewModel()
+        {
+            PlusButtonCommand = new RelayCommand(p =>
+            {
+                var view = new MusicDownloadingUC();
+                view.Margin = new Thickness(10, 10, 0, 0);
+                MyWrapPanel.Children.Add(view);
+            });
+        }
+
+    }
+}
---
+++ MusicApplication/ViewModels/MusicDownloadingUCViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicApplication.ViewModels
+{
+    public class MusicDownloadingUCViewModel : BaseViewModel
+    {
+    }
+}
